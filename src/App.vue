258c4287968f55ca299a109ae588d9a491821b81
TreleaniFederico <treleani.federico@gmail.com>
2022-09-30 00:19:23 +0200

    add bootstrap to col and row

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <div>
       <input type="text" v-model="query"> <button @click="search">Cerca</button>
     </div>
+    <div class="container">
+      <div class="row">
+        <div v-for="movie in movies" :key="movie.id" class="col-3 p-1 m-2 border border-danger border-2 rounded-3">
+          <p>Titolo: {{ movie.title }} </p>
+          <p>Titolo Originale: {{ movie.original_title }} </p>
+          <p>Lingua Originale: {{ movie.original_language }} </p>
+          <p>Rating: {{ movie.vote_average }} </p>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
