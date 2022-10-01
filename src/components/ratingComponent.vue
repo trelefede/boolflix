c99f2d4a816b975cd97218cac8359c6862b7994c
TreleaniFederico <treleani.federico@gmail.com>
2022-10-02 00:16:10 +0200

    add methods to print stars

--- src/components/ratingComponent.vue
+++ src/components/ratingComponent.vue
@@ -1,20 +1,47 @@
 <template>
     <p>
-        Voto: {{ rating }} 
-        <font-awesome-icon icon="fa-solid fa-star" />
-        <font-awesome-icon icon="fa-regular fa-star" />
+        Voto: {{ this.rate }}
+        <font-awesome-icon icon="fa-solid fa-star" v-for="fullStar in this.fullStars" :key="fullStar" class="star" />
+        <font-awesome-icon icon="fa-regular fa-star" v-for="emptyStar in this.emptyStars" :key="emptyStar" />
     </p>
 </template>
 
 <script>
 export default {
     name: "ratingComponent",
+    data(){
+        return{
+            fullStars: this.getArrayFullStars(),
+            emptyStars: this.getArrayEmptyStars()
+        }
+    },
     props: {
-        rating: String 
-    }
+        rating: Number 
+    },
+    methods:{
+        getArrayFullStars(){
+            const array = [];
+            const rating = Math.ceil(this.rating / 2);
+            for(let i = 0; i < rating; i++){
+                array.push(i);
+            }
+            return array;
+        },
+        getArrayEmptyStars(){
+            const array = [];
+            const rating = ( 5 - (Math.ceil(this.rating / 2)));
+            for(let i = 0; i < rating; i++){
+                array.push(i);
+            }
+            return array;
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+.star{
+    color: #fb0;
+}
 
 </style>
