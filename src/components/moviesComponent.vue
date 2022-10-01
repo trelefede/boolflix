c730e51dea8cd3098b538e3bfebbd6ebd88664c4
TreleaniFederico <treleani.federico@gmail.com>
2022-10-01 11:56:10 +0200

    add components

---
+++ src/components/moviesComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <h2>FILM</h2>
+        <div v-for="movie in movies" :key="movie.id" class="col-3 p-1 m-2 border border-danger border-2 rounded-3">
+          <p>Titolo: {{ movie.title }} </p>
+          <p>Titolo Originale: {{ movie.original_title }} </p>
+          <p>Lingua Originale: 
+            <span class="fi fi-" :class="movie.original_language"></span> 
+          </p>
+          <p>Rating: {{ movie.vote_average }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "moviesComponent",
+    props: {
+        movies: Array,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/seriesComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <h2>SERIES</h2>
+        <div v-for="serie in series" :key="serie.id" class="col-3 p-1 m-2 border border-danger border-2 rounded-3">
+          <p>Titolo: {{ serie.name }} </p>
+          <p>Titolo Originale: {{ serie.original_name }} </p>
+          <p>Lingua Originale: 
+            <span class="fi fi-" :class="serie.original_language"></span> 
+          </p>
+          <p>Rating: {{ serie.vote_average }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "seriesComponent",
+    props: {
+        series: Array,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
