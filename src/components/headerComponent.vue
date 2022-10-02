d5cc4eefd1c1bf715bf2a253233f3c7d932cdddc
TreleaniFederico <treleani.federico@gmail.com>
2022-10-02 22:29:16 +0200

    add header style

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,9 +1,15 @@
 <template>
-    <header>
-        <input type="text" 
-            v-model="query" 
-            @keyup="search" 
-            placeholder="Cerca...">
+    <header class="d-flex justify-content-between align-items-center px-4 bg-dark">
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo">
+        </div>
+        <div class="search">
+            <input type="text" 
+                v-model="query" 
+                @keyup="search" 
+                placeholder="Cerca...">
+        </div>
+       
     </header>
 </template>
 
@@ -24,5 +30,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+header{
+    height: 50px;
+}
+img{
+    width: 10%;
+}
 </style>
